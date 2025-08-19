f03b884049e81c976f4855b32035dc9a3dbb0527
siddharth12-21 <siddharth.ramesh1221@gmail.com>
2025-08-19 09:27:04 -0700

    Add files via upload

---
+++ Math-Variables-Intro-main (3)/Math-Variables-Intro-main/Math-Variables-Intro-main/Math-Variables-Intro-main/Main.java
@@ -0,0 +1,59 @@
+class Main {
+  public static void main(String[] args) {
+
+    //Use the // to create single line comments to either add info or to take out code for debugging
+
+    System.out.println("Hello");
+    System.out.println("world! " + 21);
+    System.out.print("Hello");
+    System.out.print(" pmorant!");
+    System.out.println();
+
+    //We will be using System.out.println and System.out.print throughout the year. Try using both below to see what the difference is!
+    //ANSWER: println automatically goes to the next line while print continues on the same line
+
+    //Throughout the year this year we will need to store information. For that we will be using VARIABLES!
+    //Java is an Object-Oriented programming language. All variables we use this year will either be OBJECTS or PRIMITIVES
+    //There are 8 primitives in Java: int, byte, short, long, float, double, boolean, char
+    //For AP we need to know: int, double, boolean
+    //List examples of the types below and give definition
+    //int (integer): 
+    //double:
+    //boolean:
+    //For now we are just going to work with primitive
+    //Create 3 variables of each of the above types (USE GOOD CODING PRACTICE WHEN CREATING THE VARIABLES
+
+    int count = 21;
+    int c = 0;
+    double ba = 1.7;
+    boolean isTrue = true;
+
+
+
+  for (int val = 0; val<=4;val++){
+    System.out.println(val);
+  }
+    
+
+
+    //MATH TIME!
+
+    //What are the math operators that we can use?
+    //+ - * / %
+
+    //Try doing some math operations with numbers. How can we check to see if the math worked?
+
+    //Create codes that will print the following:
+
+    //Odd integers from 1 to 100, inclusive of both
+
+
+
+    //All multiples of 3 from 1 to 100
+
+
+
+
+    //Starting at 1000, print on the same line (with a - [hypthen] between each) all of the numbers that end in 0 going down to 0
+  }
+}
